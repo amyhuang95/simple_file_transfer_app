729549369f297d30f5f4e22311fd09a77ad8b1c5
amyhuang95 <62624198+amyhuang95@users.noreply.github.com>
2025-01-17 21:49:24 -0800

    handles concurrent requests

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import socket  # Create TCP connection
+import threading  # Implement concurrency
 
 # Set up server socket using TCP/IP
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -9,9 +10,8 @@ server_socket.bind(('0.0.0.0', 8888))
 server_socket.listen(5)  # up to 5 clients allowed in the queue
 print(f'Server listening on port {port}...')
 
-# Process request
-while True:
-    client_connection, client_address = server_socket.accept()
+
+def handle_client(client_connection, client_address):
     print(f'Connected to {client_address}')
     file_request = client_connection.recv(1024).decode()
 
@@ -21,3 +21,11 @@ while True:
                 client_connection.sendall(data_chunk)
     except FileNotFoundError:
         client_connection.send(b"File not found")  # byte string
+
+
+# Process request
+while True:
+    client_connection, client_address = server_socket.accept()
+    thread = threading.Thread(target=handle_client, args=(
+        client_connection, client_address))
+    thread.start()
