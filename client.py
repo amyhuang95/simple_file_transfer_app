2f36f346c7939b6365b4643968baae43c296a468
amyhuang95 <62624198+amyhuang95@users.noreply.github.com>
2025-01-17 21:38:16 -0800

    add file request and receiving data

--- client.py
+++ client.py
@@ -2,3 +2,18 @@ import socket  # create TCP connection
 
 # Set up connection to the server
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+port = 8888
+client_socket.connect(('localhost', port))
+
+# Request a file
+file_request = 'test.txt'
+client_socket.send(file_request.encode())
+
+# Receive data
+with open('downloaded_file.txt', 'wb') as file:
+    while True:
+        data = client_socket.recv(1024)
+        if not data:
+            break
+        file.write(data)
+print('File successfully received!')
