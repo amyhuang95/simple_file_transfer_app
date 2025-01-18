346a5cc42459bc4a2a851166942d8caae56cc829
amyhuang95 <62624198+amyhuang95@users.noreply.github.com>
2025-01-17 22:46:50 -0800

    refactor inputs with variables

--- server.py
+++ server.py
@@ -1,31 +1,41 @@
 import socket  # Create TCP connection
 import threading  # Implement concurrency
 
-# Set up server socket using TCP/IP
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-port = 8888
-server_socket.bind(('0.0.0.0', 8888))
 
-# Listening for incoming connections
-server_socket.listen(5)  # up to 5 clients allowed in the queue
-print(f'Server listening on port {port}...')
-
-
+# Process client request in multithreading function
 def handle_client(client_connection, client_address):
     print(f'Connected to {client_address}')
-    file_request = client_connection.recv(1024).decode()
+    buffer_size = 1024
+    # read server's reply
+    file_request = client_connection.recv(buffer_size).decode()
 
     try:
         with open(file_request, 'rb') as file:
             for data_chunk in file:
+                # note data_chunk is in binary format
                 client_connection.sendall(data_chunk)
             file.close()
     except FileNotFoundError:
         client_connection.send(b"File not found")  # byte string
 
 
-# Process request
+# Set up server socket using TCP/IP
+internet_address_family = socket.AF_INET  # IPv4
+socket_type = socket.SOCK_STREAM  # TCP
+server_socket = socket.socket(internet_address_family, socket_type)
+
+# Associate the socket with a specific network
+host = 'localhost'
+port = 8888
+server_socket.bind((host, port))
+
+# Listening for incoming connections
+server_socket.listen()
+print(f'Server listening on port {port}...')
+
+# Process request using multithreading
 while True:
+    # waits for incomming connection and accept it
     client_connection, client_address = server_socket.accept()
     thread = threading.Thread(target=handle_client, args=(
         client_connection, client_address))
